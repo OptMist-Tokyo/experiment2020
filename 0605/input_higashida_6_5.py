ab47009d39c121b08c944f92fa9a0e6cf31dab93
Mizuki-Higashida <65003543+Mizuki-Higashida@users.noreply.github.com>
2020-06-05 15:59:06 +0900

    隣に座りたい人のデータNextToを含む入力に対応
    NextToに書かれた名前から学生の番号を調べる関数find_numも追加しておきました．

--- 0605/input_higashida_6_5.py
+++ 0605/input_higashida_6_5.py
@@ -1,17 +1,15 @@
-import sys
-
 '''
 入力形式：
 1行目：座席の行数，列数
 2行目：学生の総数
 3行目以降：
 各行ごとに
-    名前と選好度(ここでは1～5とする)
+    名前と選好度(ここでは1～5とする), 隣に座りたい人（希望があればその人の名前，なければ-1が入る）
 
 以下，.txtでの読み込みを想定
 '''
 
-info = ('name','Blackboard','Window','Airconditioner','Edge') #選好に対応する命名
+info = ('name', 'Blackboard', 'Window','Airconditioner','Edge','NextTo') #選好に対応する命名
 
 #rawのデータをsummarizedにまとめる
 def summarize(raw):
@@ -24,12 +22,26 @@ def summarize(raw):
         #1行ずつ全員分の個人データを読み込む
         for i in range(total):
             data = f.readline().split()
-            data[1:] = map(int, data[1:]) #先頭のname以外の要素をint型に変換
+            data[1:5] = map(int, data[1:5]) #先頭のname, 末尾のNextTo以外の要素をint型に変換
             students.append(dict(zip(info, data))) #infoをkeyにして個人データを1人分ずつ辞書にまとめる
 
     summarized = (row, column, total, students)
     return summarized
 
-#raw_dataを受け取り，データをsummaized_dataにまとめる
-raw_data = sys.argv[1] #サンプルデータはコマンドラインで指定
+#summarized_dataにおいて，指定された番号num_of_studentの学生について，そのNextToに対応する学生の番号を返す
+#nameの値が重複する学生はいないことを仮定
+def find_num(num_of_student):
+    i = 0
+    nextto = summarized_data[3][num_of_student]['NextTo']
+    while i < summarized_data[2]:
+        if summarized_data[3][i]['name'] == nextto:
+            return i #もし見つかれば番号を返す
+        i += 1
+    return -1 #見つからなければ-1を返す
+
+
+#raw_dataを受け取り，データをsummarized_dataにまとめる
+raw_data = 'input_sample_full1.txt' #サンプルデータ
 summarized_data = summarize(raw_data)
+
+#NextToの値-1もstr型であることに注意
